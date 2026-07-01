df26fa87a073434aaab5c3152383435c04f50ba0
Orestis Apostolou <cp40061@gmail.com>
2026-07-01 12:58:09 +0300

    updated heat bars for UI

--- topdownshooter/Assets/Scripts/UI/HeatBar.cs
+++ topdownshooter/Assets/Scripts/UI/HeatBar.cs
@@ -4,19 +4,12 @@ using UnityEngine.UI;
 public class HeatBar : MonoBehaviour
 {
     public HeatSystem heatSystem;
-    private Slider slider;
     public Image fillImage;
     public Gradient gradient;
-    private void Awake()
-    {
-        slider = GetComponent<Slider>();
-    }
-
     void Update()
     {
         float percent = heatSystem.HeatPercent();
-        slider.value = percent;
-
+        fillImage.fillAmount = percent;
         fillImage.color = gradient.Evaluate(percent);
     }
 }
